3375045720ff7f10a901f1963a2c531ebe75500c
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-09 04:44:16 -0700

    calender program is added

---
+++ Calender/Calender/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Calender");
+            CalenderDisplay.PrintCalender();
+
+        }
+    }
+}
