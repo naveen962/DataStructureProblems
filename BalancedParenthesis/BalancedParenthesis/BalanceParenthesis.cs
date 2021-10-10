b9830c9461a7d93c66dc52045723635b3db4a9bc
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-10 00:49:09 -0700

    Balenced parenthesis program is added

---
+++ BalancedParenthesis/BalancedParenthesis/BalanceParenthesis.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BalancedParenthesis
+{
+    class BalanceParenthesis
+    {
+        public void IsBalanceParentheses()
+        {
+            bool valid = false;
+            Console.WriteLine("Enter Arithmetic Expression:");
+            string expression = Console.ReadLine();
+            char[] charArray = expression.ToCharArray();
+            int size = charArray.Length;
+            for (int j = 0; j < size; j++)
+            {
+                if (charArray[j] == '(' || charArray[j] == '{' || charArray[j] == '[' || charArray[j] == ')' || charArray[j] == '}' || charArray[j] == ']')
+                {
+                    valid = true;
+                    break;
+                }
+            }
+            if (valid)
+            {
+                StackArray stack = new StackArray(size);
+                int i = 0;
+                while (i < size)
+                {
+                    if (charArray[i] == '(' || charArray[i] == '{' || charArray[i] == '[')
+                    {
+                        stack.Push(charArray[i]);
+                    }
+                    else if (charArray[i] == ')' || charArray[i] == '}' || charArray[i] == ']')
+                    {
+                        stack.Pop();
+                    }
+                    i++;
+                }
+                if (stack.IsEmpty())
+                {
+                    Console.WriteLine("parentheses appeared in a balanced fashion");
+                }
+                else
+                {
+                    Console.WriteLine("parentheses are not appeared in a balanced fashion");
+                }
+            }
+            else
+            {
+                Console.WriteLine("there is no parentheses!!!");
+            }
+        }
+    }
+}
---
+++ BalancedParenthesis/BalancedParenthesis/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BalanceParenthesis bp = new BalanceParenthesis();
+            bp.IsBalanceParentheses();
+        }
+    }
+}
