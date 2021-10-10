c60b1cc7383fe1257b63daf837136596930bd6d4
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-10 02:31:02 -0700

    unorderedlist program is added

---
+++ UnorderedList/UnorderedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace UnorderedList
+{
+    class Program
+    {
+       
+           static void Main(string[] args)
+           {
+
+            string path = @"F:\JSON\test.txt";
+            ReadAllText text = new ReadAllText();
+            text.Read(path);
+
+
+           }
+               
+          
+        
+    }
+}
---
+++ UnorderedList/UnorderedList/ReadAllText.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace UnorderedList
+{
+    class ReadAllText
+    {
+        public void Read(string path)
+        {
+            string line;
+            try
+            {
+                line = File.ReadAllText(path);
+                Console.WriteLine(line);
+                string[] wordlist = line.Split();
+                LinkedList<string> list = new LinkedList<string>();
+                foreach (string ele in wordlist)
+                {
+                    list.AddFirst(ele);
+                }
+
+                foreach (string ele in list)
+                {
+                    Console.WriteLine(ele);
+                }
+            }
+
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
+
+        }
+    }
+}
